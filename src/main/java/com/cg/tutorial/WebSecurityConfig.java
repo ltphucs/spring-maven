7fab35ff2c47fbd300b3081b967dafa06f0daffb
Le Tuong Phuc <ltphuc@gmail.com>
2020-03-18 16:43:53 +0700

    Spring security 4.0

---
+++ src/main/java/com/cg/tutorial/WebSecurityConfig.java
@@ -0,0 +1,61 @@
+package com.cg.tutorial;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+
+        @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    };
+
+
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return  authenticationProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().antMatchers("/admin/*").hasRole("ADMIN")
+                .and()
+                .authorizeRequests().antMatchers("/user/*").hasAnyRole("ADMIN", "USER")
+                .and()
+                .authorizeRequests().antMatchers("/","/home","/assets-admin/**","/upload/**").permitAll()
+                .and()
+                .authorizeRequests().anyRequest().authenticated()
+                //.antMatchers("/","/index","/home","/assets-admin/*","/upload/*").permitAll()
+                .and().formLogin()
+                .loginPage("/user/login")
+                .usernameParameter("username").passwordParameter("password")
+                .loginProcessingUrl("/user/postLogin").permitAll()
+                .successHandler(customAuthenticationSuccessHandler)
+                .failureUrl("/user/loginFailed").and().logout().logoutUrl("/user/doLogout")
+                .logoutSuccessUrl("/user/logout").permitAll()
+                .and().csrf().disable();
+    }
+}
